e5cd29d859798dfca07c1670f5802fc92fb0fab1
zhangmaolin <zhangmaolin8888@163.com>
2026-04-06 09:31:20 +0800

    hhh

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package com.example;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.*"})
+//@ComponentScan(basePackages = {"com.example.*"})
+@MapperScan("com.example.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
-package com.example.service;
+package com.example.service.impl;
 
 import com.example.entity.User;
 import com.example.mapper.UserMapper;
+import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
